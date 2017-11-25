c65b6bb7f4f73f35f1eb298a73ecabfe65b03515
Felix Breidenstein <mail@felixbreidenstein.de>
2017-11-25 17:01:29 +0100

    Implemented the DRF quickstart

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from backend.api.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
