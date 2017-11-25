ebc851379199fb2fcb1ecf8edbfdc5b3c202805d
Niko Wenselowski <der@nik0.de>
2017-11-25 22:23:03 +0100

    Make the ethnos optional.

--- backend/api/models.py
+++ backend/api/models.py
@@ -34,7 +34,7 @@ class Character(models.Model):
     name = models.CharField(max_length=50)
     streetname = models.CharField(max_length=50)
     metatype = models.CharField(max_length=20, choices=RACES)
-    ethnos = models.CharField(max_length=50)
+    ethnos = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.user)
