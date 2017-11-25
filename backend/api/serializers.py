ab8ded753f20c47f5a6b21b6317d396360dfe2fc
Niko Wenselowski <der@nik0.de>
2017-11-25 22:43:39 +0100

    Make sure the ID is displayed.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,22 +5,22 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'lang', 'character_set')
+        fields = ('id', 'url', 'username', 'email', 'lang', 'character_set')
 
 
 class CharacterSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Character
-        fields = '__all__'
+        fields = ('id', 'url', 'name', 'streetname', 'metatype', 'ethnos', 'user', 'group')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ('url', 'name', 'character_set')
+        fields = ('id', 'url', 'name', 'character_set')
 
 
 class RunSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Run
-        fields = '__all__'
+        fields = ('id', 'name', 'gamemaster', 'group')
